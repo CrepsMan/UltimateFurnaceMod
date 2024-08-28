3857012f98aab01a2ffb4fb8b50bb6f38fc38888
CrepsMan <eggenmattias+GitHub@gmail.com>
2024-08-28 23:32:40 +0200

    mod work now, (a bit bug testing needed for the upgrading of the furnace.) (smeltCount). Smelting works. Smelting progress arrow works. Smelting fuel time left progress fire works.
    Signed-off-by: CrepsMan <eggenmattias+GitHub@gmail.com>

--- src/main/java/com/CrepsMan/ultimate_furnace/screen/UltimateFurnaceScreenHandlerGum.java
+++ src/main/java/com/CrepsMan/ultimate_furnace/screen/UltimateFurnaceScreenHandlerGum.java
@@ -3,17 +3,63 @@ package com.CrepsMan.ultimate_furnace.screen;
 import com.CrepsMan.ultimate_furnace.registry.ModScreenHandlers;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.recipe.book.RecipeBookCategory;
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
 import net.minecraft.screen.PropertyDelegate;
 
 public class UltimateFurnaceScreenHandlerGum extends AbstractFurnaceScreenHandler {
+	private final PropertyDelegate customPropertyDelegate;
+
 	public UltimateFurnaceScreenHandlerGum(int syncId, PlayerInventory playerInventory) {
-		super(ModScreenHandlers.ULTIMATE_FURNACE_SCREEN_HANDLER_GUM, RecipeType.SMELTING, RecipeBookCategory.FURNACE, syncId, playerInventory);
+		this(syncId, playerInventory, new SimpleInventory(3), new PropertyDelegate() {
+			private final int[] data = new int[5];
+
+			@Override
+			public int get(int index) {
+				return data[index];
+			}
+
+			@Override
+			public void set(int index, int value) {
+				data[index] = value;
+			}
+
+			@Override
+			public int size() {
+				return data.length;
+			}
+		});
 	}
 
 	public UltimateFurnaceScreenHandlerGum(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate propertyDelegate) {
 		super(ModScreenHandlers.ULTIMATE_FURNACE_SCREEN_HANDLER_GUM, RecipeType.SMELTING, RecipeBookCategory.FURNACE, syncId, playerInventory, inventory, propertyDelegate);
+		this.customPropertyDelegate = propertyDelegate;
+		this.addProperties(this.customPropertyDelegate);
+	}
+
+	// Get the current cook progress for rendering the cook progress bar
+	public int getCookProgress() {
+		int cookTime = this.customPropertyDelegate.get(2);
+		int cookTimeTotal = this.customPropertyDelegate.get(3);
+		return cookTimeTotal != 0 && cookTime != 0 ? cookTime * 24 / cookTimeTotal : 0;
+	}
+
+	// Get the current fuel progress for rendering the flame icon
+	public int getFuelProgress() {
+		int burnTime = this.customPropertyDelegate.get(0);
+		int currentBurnTime = this.customPropertyDelegate.get(1);
+		return currentBurnTime != 0 && burnTime != 0 ? burnTime * 13 / currentBurnTime : 0;
+	}
+
+	// Get whether the furnace is currently burning
+	public boolean isBurning() {
+		return this.customPropertyDelegate.get(0) > 0;
+	}
+
+	// Get the smelt count to be displayed in the GUI
+	public int getSmeltCount() {
+		return this.customPropertyDelegate.get(4);
 	}
 }
