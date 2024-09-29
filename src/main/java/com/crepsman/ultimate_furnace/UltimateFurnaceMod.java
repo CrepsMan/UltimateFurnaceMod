7ffbe298507b9c2d97fe09f3338cab5710d4d1e5
CrepsMan <crepsman1+GitHub@gmail.com>
2024-09-29 22:17:01 +0200

    Bug fixe

--- src/main/java/com/crepsman/ultimate_furnace/UltimateFurnaceMod.java
+++ src/main/java/com/crepsman/ultimate_furnace/UltimateFurnaceMod.java
@@ -1,14 +1,14 @@
 package com.crepsman.ultimate_furnace;
 
 import com.crepsman.ultimate_furnace.blocks.entity.UltimateFurnaceBlockEntity;
-import com.crepsman.ultimate_furnace.registry.*;
+import com.crepsman.ultimate_furnace.registry.ModBlockEntities;
+import com.crepsman.ultimate_furnace.registry.ModBlocks;
+import com.crepsman.ultimate_furnace.registry.ModScreenHandlers;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
-import net.minecraft.command.CommandSource;
 import net.minecraft.command.argument.BlockPosArgumentType;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
@@ -25,46 +25,46 @@ public class UltimateFurnaceMod implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("Ultimate Furnace");
 	public static final String MOD_ID = "ultimate_furnace";
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing Ultimate Furnace!");
 
-
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
 			dispatcher.register(literal("setsmeltcount").requires(source -> source.hasPermissionLevel(2))
-				.then(argument("position", BlockPosArgumentType.blockPos()))
-				.then(argument("set", IntegerArgumentType.integer(0, 15000)))
-					.executes(context -> {
-						final int set = IntegerArgumentType.getInteger(context, "set");
-						final BlockPos pos = BlockPosArgumentType.getBlockPos(context, "position");
+				.then(argument("position", BlockPosArgumentType.blockPos())
+					.then(argument("set", IntegerArgumentType.integer(0, 15000))
+						.executes(context -> {
+							final int set = IntegerArgumentType.getInteger(context, "set");
+							final BlockPos pos = BlockPosArgumentType.getBlockPos(context, "position");
 
-						Block block = context.getSource().getWorld().getBlockState(pos).getBlock();
-						if(block == ModBlocks.ULTIMATE_FURNACE){
-							UltimateFurnaceBlockEntity entity = context.getSource().getWorld().getBlockEntity(pos, ModBlockEntities.ULTIMATE_FURNACE_BLOCK_ENTITY).get();
-							entity.setSmeltCount(set);
-							context.getSource().sendFeedback(Text.translatable("commands.ultimate_furnace.ultimate_furnace.set", new Object[]{entity.getSmeltCount()}), true);
-						} else {
-							context.getSource().sendFeedback(Text.translatable("commands.ultimate_furnace.ultimate_furnace.fail"), false);
-						}
-						return 1;
-			})));
+							Block block = context.getSource().getWorld().getBlockState(pos).getBlock();
+							if (block == ModBlocks.ULTIMATE_FURNACE) {
+								UltimateFurnaceBlockEntity entity = context.getSource().getWorld().getBlockEntity(pos, ModBlockEntities.ULTIMATE_FURNACE_BLOCK_ENTITY).get();
+								entity.setSmeltCount(set);
+								context.getSource().sendFeedback(Text.translatable("commands.ultimate_furnace.ultimate_furnace.set", entity.getSmeltCount()), true);
+							} else {
+								context.getSource().sendFeedback(Text.translatable("commands.ultimate_furnace.ultimate_furnace.fail"), false);
+							}
+							return 1;
+						})))
+			));
+
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
 			dispatcher.register(literal("getsmeltcount")
-				.then(argument("position", BlockPosArgumentType.blockPos()))
+				.then(argument("position", BlockPosArgumentType.blockPos())
 					.executes(context -> {
 						final BlockPos pos = BlockPosArgumentType.getBlockPos(context, "position");
 						Block block = context.getSource().getWorld().getBlockState(pos).getBlock();
-						if(block == ModBlocks.ULTIMATE_FURNACE){
+						if (block == ModBlocks.ULTIMATE_FURNACE) {
 							UltimateFurnaceBlockEntity entity = context.getSource().getWorld().getBlockEntity(pos, ModBlockEntities.ULTIMATE_FURNACE_BLOCK_ENTITY).get();
-
-							context.getSource().sendFeedback(Text.translatable("commands.ultimate_furnace.ultimate_furnace.get", new Object[]{entity.getSmeltCount()}), true);
+							context.getSource().sendFeedback(Text.translatable("commands.ultimate_furnace.ultimate_furnace.get", entity.getSmeltCount()), true);
 						} else {
 							context.getSource().sendFeedback(Text.translatable("commands.ultimate_furnace.ultimate_furnace.fail"), false);
 						}
-
 						return 1;
-			})));
-
+					}))
+			));
 
 		ModBlocks.registerModBlocks();
 		ModBlockEntities.registerBlockEntities();
@@ -74,6 +74,4 @@ public class UltimateFurnaceMod implements ModInitializer {
 	public static void addItemsToItemGroup(QuiltItemGroup entries) {
 		entries.appendStacks(DefaultedList.ofSize(1, new ItemStack(ModBlocks.ULTIMATE_FURNACE)));
 	}
-
-
 }
